a4b435eab1495a57404403cbc4b8526072ab793a
code-walker-23 <chhabravinay549@gmail.com>
2024-08-16 20:59:13 +0530

    created-routing config. and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1 className='bg-white text-black rounded-lg shadow-lg'>Hello World!</h1>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ForgotPassword() {
+  return <div>
+    Forgot password
+  </div>;
+}
+
+export default ForgotPassword;
