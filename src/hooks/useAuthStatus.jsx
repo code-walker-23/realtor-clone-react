a7924135460df406da161ceb317fff12d673fc21
code-walker-23 <chhabravinay549@gmail.com>
2024-08-20 00:43:32 +0530

    reusing authStatus hook

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import { auth } from "../utils/firebase";
+
 function useAuthStatus() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setLoggedIn(true);
+      } else {
+        setLoggedIn(false); 
       }
       setCheckingStatus(false);
     });
     return () => unsubscribe();
   }, []);
+
   return { loggedIn, checkingStatus };
 }
 
